0e793293133a2a4a74d96b8e669d9a26467e838d
Ravi-Chavda <rc5501@nyu.edu>
2024-07-29 20:18:05 +0000

    updated index file

--- service/static/js/rest_api.js
+++ service/static/js/rest_api.js
@@ -8,23 +8,23 @@ $(function () {
     function update_form_data(res) {
         $("#product_id").val(res.id);
         $("#product_name").val(res.name);
+        $("#product_description").val(res.description);
         $("#product_quantity").val(res.quantity);
+        $("#product_price").val(res.price);
+        $("#product_product_id").val(res.product_id);
         $("#product_restock_level").val(res.restock_level);
-        $("#product_restock_count").val(res.restock_count);
         $("#product_condition").val(res.condition);
-        $("#product_first_entry_date").val(res.first_entry_date);
-        $("#product_last_restock_date").val(res.last_restock_date);
     }
 
-    /// Clears all form fields
+    // Clears all form fields
     function clear_form_data() {
         $("#product_name").val("");
+        $("#product_description").val("");
         $("#product_quantity").val("");
+        $("#product_price").val("");
+        $("#product_product_id").val("");
         $("#product_restock_level").val("");
-        $("#product_restock_count").val("");
         $("#product_condition").val("Unknown");
-        $("#product_first_entry_date").val("");
-        $("#product_last_restock_date").val("");
     }
 
     // Updates the flash message area
@@ -38,9 +38,11 @@ $(function () {
     // ****************************************
 
 
+
     // ****************************************
     // Update a Product
     // ****************************************
+
 
     // ****************************************
     // Retrieve a Product
@@ -63,7 +65,8 @@ $(function () {
     });
 
     // ****************************************
-    // Search for a Product
+    // Search for Products
     // ****************************************
+
 
 })
