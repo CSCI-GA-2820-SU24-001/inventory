cd5d7213ee046f14ac78ce616d8291b0307159bf
Yidan Qin <oliviaworks1999@gmail.com>
2024-08-03 22:25:29 -0700

    UI to read/retrieve an inventory item

--- feature/inventory.feature
+++ feature/inventory.feature
@@ -86,3 +86,15 @@ Feature: Inventory Management
       | 34  | laptop | Electronics | 25       | 1000  | 1          | 10            | NEW       |
       | 890 | chair  | Furniture   | 75       | 150   | 3          | 15            | USED      |
       | 456 | marker | Stationery  | 18       | 1     | 4          | 18            | USED      |
+  
+  Scenario: Read an inventory item
+    When I visit the "Home Page"
+    And I set the "product_id" to 3
+    And I press the "Retrieve" button
+    Then I should see the message "Success"
+    And I should see "chair" in the results
+    And I should see "Furniture" in the results
+    And I should see "75" in the results
+    And I should not see "laptop" in the results
+    And I should not see "tablet" in the results
+    And I should not see "marker" in the results
--- service/static/js/rest_api.js
+++ service/static/js/rest_api.js
@@ -84,7 +84,31 @@ $(function () {
     // ****************************************
     // Retrieve a Product
     // ****************************************
+    $("#retrieve-btn").click(function () {
 
+        let product_id = $("#product_id").val();
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "GET",
+            url: `/inventory/${product_id}`,
+            contentType: "application/json",
+            data: ''
+        })
+
+        ajax.done(function(res){
+            //alert(res.toSource())
+            update_form_data(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            clear_form_data()
+            flash_message(res.responseJSON.message)
+        });
+
+    });
 
     // ****************************************
     // Delete a Product
